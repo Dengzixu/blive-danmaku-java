443880144bf3158d854e09b8b8cfbc897b753476
Deng.Zixu <admin@dengzixu.com>
2023-09-05 03:13:48 +0800

    🧪 修改 Packet 相关测试用例

--- src/test/java/PacketTest.java
+++ src/test/java/PacketTest.java
@@ -4,25 +4,43 @@ import net.dengzixu.bilvedanmaku.enums.Operation;
 import net.dengzixu.bilvedanmaku.enums.ProtocolVersion;
 import net.dengzixu.bilvedanmaku.packet.Packet;
 import net.dengzixu.bilvedanmaku.packet.PacketBuilder;
+import net.dengzixu.bilvedanmaku.packet.PacketResolver;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
 public class PacketTest {
     @Test
-    void TestPacketBuilder() {
-
-        BiliBiliLiveAPI biliBiliLiveAPI = new BiliBiliLiveAPI();
-
-        String token = (String) biliBiliLiveAPI.getConf(3080147).get("token");
-
-        // 构建认证数据包
+    void testBuildPingPacket() {
         Packet packet = PacketBuilder.newBuilder()
                 .protocolVersion(ProtocolVersion.HEARTBEAT)
-                .operation(Operation.USER_AUTHENTICATION)
-                .body(new AuthBody(3080147, token).toJsonBytes())
+                .operation(Operation.HEARTBEAT)
                 .build();
 
-        System.out.println(Arrays.toString(packet.getBytes()));
+        System.out.println("ping packet (Base64): " + Base64.encodeBase64String(packet.getBytes()));
+    }
+
+    @Test
+    void testDecodeHEXPacket() {
+        String hexPacket = "00000010001000010000000200000002";
+
+        try {
+            System.out.println(new PacketResolver(Hex.decodeHex(hexPacket)).resolve());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testDecodeBase64Packet() {
+        String base64Data = "AAAAEAAQAAEAAAACAAAAAQ==";
+
+        try {
+            System.out.println(new PacketResolver(base64Data).resolve());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
